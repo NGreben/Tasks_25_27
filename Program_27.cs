86b14396b61e1592bac0b0cd87cbfd785f31c107
NGreben <96392178+NGreben@users.noreply.github.com>
2023-03-19 16:37:01 +0300

    Add files via upload

---
+++ Program_27.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out number);
+Console.Write($"{Sum(number)}");
+
+int Sum(int _number)
+{
+    int sum = 0;
+    if (_number % 10 == 0)
+        sum = _number;
+    else while (_number >= 10)
+        {
+            sum = sum + _number % 10; // 0+2
+            _number = _number / 10;// 8
+        }
+    sum = sum + _number;
+    return sum;
+}
